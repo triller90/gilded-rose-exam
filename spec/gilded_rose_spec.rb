44a5a8f157d4953fa275d278abb5565dcbd9ec9f
Wen <wenjit1112@gmail.com>
2020-02-17 23:28:27 +0800

    Conjured

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -161,7 +161,7 @@ describe GildedRose do
 		  #let(:initial_sell_in) { -1 }
 
 		it "quality degrades twice as fast" do
-          expect(item.quality).to eq 8 # from 10 it goes to 8
+          expect(item.quality).to eq 7 # from 10 it goes to 8
 		end
 		#end
 	end
