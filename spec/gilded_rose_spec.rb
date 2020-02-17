196f78dc61e91e4e42c942b316ffea6d31079dcd
Wen <wenjit1112@gmail.com>
2020-02-17 23:36:00 +0800

    Conjured

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -157,13 +157,13 @@ describe GildedRose do
 		  end
 		end
 		
-		#context "and sell in is 10 days" do
-		  #let(:initial_sell_in) { -1 }
+		context "and sell in is 10 days" do
+		  let(:initial_sell_in) { 10 }
 
-		it "quality degrades twice as fast" do
-          expect(item.quality).to eq 7 # from 10 it goes to 8
+		  it "degrades quality twice" do
+            expect(item.quality).to eq 8 # from 10 it goes to 8
+		  end
 		end
-		#end
 	end
 	#end
 	
